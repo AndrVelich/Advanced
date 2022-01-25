0ded14128ae3c95f38a34035213ac4c3b95bdfc2
Eugene <eugene.vezhnavets@gmail.com>
2022-01-25 14:24:36 +0300

    enabling Razor Component routing in the example application

--- Startup.cs
+++ Startup.cs
@@ -47,6 +47,7 @@ namespace Advanced
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
                 endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
             });
             SeedData.SeedDatabase(context);
         }
